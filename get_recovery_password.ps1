c15c969fec2c7479e26e929daef6d36ea89753ff
ambalabanov <a.balabanov@icloud.com>
2021-12-09 16:39:51 +0500

    Add tests

---
+++ get_recovery_password.ps1
@@ -0,0 +1,24 @@
+﻿Function Get-RP1($comp){
+  write-host "Search",$comp
+  try{
+    $Computer = Get-ADComputer -identity $comp -Server domain.local
+  }
+  catch{
+    return "Search error`n"
+  }
+  $obj = Get-ADObject -SearchBase $Computer.distinguishedname -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -Properties *
+ 
+  write-host "Found", ($obj | measure).Count, "password(s)"
+  write-host ""
+  foreach ($i in $obj){
+    write-host "msFVE-RecoveryGuid:`t",([guid]($i.'msFVE-RecoveryGuid'))
+    write-host "msFVE-VolumeGuid:`t",([guid]($i.'msFVE-VolumeGuid'))
+    write-host "msFVE-RecoveryPassword:`t" $i.'msFVE-RecoveryPassword'
+    write-host ""
+  }
+  
+}
+ 
+ 
+Get-RP1 ('ubuntu20')
+ 
